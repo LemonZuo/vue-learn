48937817c27438fba1e98f537a05db8c593e6188
LemonZuo <lemonzuo@qq.com>
2021-03-14 21:45:11 +0800

    组件基本使用

---
+++ vue_demo/01-src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <img class="logo" src="./assets/logo.png" alt="logo">
+    <!-- 3、使用组件标签 -->
+    <HelloWord/>
+  </div>
+</template>
+
+<script>
+// 1、引入组件
+import HelloWord from "./components/HelloWord.vue"
+
+export default {
+  name: 'App',
+  // 2、映射组件标签
+  components: {HelloWord}
+}
+</script>
+
+<style>
+.logo {
+  width: 200px;
+  height: 200px;
+}
+</style>
