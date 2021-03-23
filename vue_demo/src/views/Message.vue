1a0c39038bf5dcf073fdb711b001b4acf64ad32c
LemonZuo <lemonzuo@qq.com>
2021-03-23 22:42:25 +0800

    编程式导航

--- vue_demo/src/views/Message.vue
+++ vue_demo/src/views/Message.vue
@@ -3,8 +3,11 @@
         <ul>
             <li v-for="(message, index) in messageList" :key="message.id">
                 <router-link :to="`/home/message/messageDetail/${message.id}`">{{ message.title }}</router-link>
+                <button @click="pushShow(message.id)">push查看</button>
+                <button @click="replaceShow(message.id)">replace查看</button>
             </li>
         </ul>
+        <button @click="$router.back()">回退</button>
         <hr>
         <div>
             <router-view></router-view>
@@ -29,6 +32,12 @@ export default {
             ];
             this.messageList = message;
         }, 500);
+    }, methods: {
+        pushShow(id) {
+            this.$router.push(`/home/message/messageDetail/${id}`)
+        }, replaceShow(id) {
+            this.$router.replace(`/home/message/messageDetail/${id}`)
+        }
     }
 }
 </script>
